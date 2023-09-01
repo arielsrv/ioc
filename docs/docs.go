fea5908db690ef8fb1d44c3528042138fe2c71ee
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-09-01 15:19:08 -0300

    update

--- docs/docs.go
+++ docs/docs.go
@@ -50,7 +50,7 @@ const docTemplate = `{
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "localhost:8080",
-	BasePath:         "/.",
+	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Fiber Example API",
 	Description:      "This is a sample swagger for Fiber",
